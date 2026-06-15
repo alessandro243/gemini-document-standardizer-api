0d99ab035314952275bb2e9c27ad4d4438b1461d
alessandro243 <alex.alessandro243@gmail.com>
2026-06-15 14:42:18 -0300

    feat: add extraction of report header metadata

---
+++ src/main/java/dev/alex/standardizer/config/PromptProperties.java
@@ -0,0 +1,12 @@
+package dev.alex.standardizer.config;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PromptProperties {
+    private String promptDir = "C:\\Users\\alexa\\Desktop\\spring projects\\gemini-document-standardizer-api-main\\gemini-document-standardizer-api-main\\src\\main\\resources\\prompt.txt";;
+}
