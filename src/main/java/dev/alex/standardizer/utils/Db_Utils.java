240bae15ef7a646b349267ad77dc596eb17f7073
alessandro243 <alex.alessandro243@gmail.com>
2026-08-17 04:00:58 -0300

    feat: implement functional pipeline for data reconciliation

--- src/main/java/dev/alex/standardizer/utils/Db_Utils.java
+++ src/main/java/dev/alex/standardizer/utils/Db_Utils.java
@@ -15,7 +15,6 @@ public class Db_Utils {
 
     public List<Map<String, Object>> selectFinalReport(Db_Utils database, String report_id){
         String sql = "SELECT unidade_auditora, loja_origem, volume_total_pecas, valor_total_retido, atraso_medio_dias FROM inventario_consolidado where unidade_auditora like ?";
-        System.out.println(report_id + "<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         return selector.queryForList(sql, "%" + report_id + "%");
     }
 
